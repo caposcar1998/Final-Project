be56c9f66342f596f92b396a0c51c3cd0b1c8ff3
Manue Ortiz Hernández <manuoh09@gmail.com>
2021-06-02 18:44:53 -0500

    Emitter and Particles added

---
+++ Assets/Scripts/Emitter.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Emitter : MonoBehaviour
+{
+    public GameObject ParticlePrefab; 
+    public int numParticles;
+    private GameObject[] particles;
+    // Start is called before the first frame update
+    
+    void Start()
+    {
+        particles = new GameObject[numParticles];
+
+        for(int p = 0; p < numParticles; p++){
+            GameObject part = Instantiate(ParticlePrefab);
+            Particle pScript = part.GetComponent<Particle>(); 
+            // + Each particle will have a random radius in the range [0.3, 0.9]
+            float diam = Random.Range(0.3f, 0.9f)*2; 
+            pScript.r = diam/2.0f;
+            part.transform.localScale = new Vector3(diam, diam, diam);
+
+            // + Each particle will have a random material color.
+            Vector3 materialColors = new Vector3(Random.Range(0.2f, 0.7f), /* - The red diffuse channel in the range [0.2, 0.7] */
+                                                Random.Range(0.2f, 1.0f), /* - The green diffuse channel in the range [0.2, 1.0] */
+                                                Random.Range(0.2f, 1.0f)); /* - The blue diffuse channel in the range [0.2, 1.0] */
+            Color c = new Color(materialColors.x,materialColors.y, materialColors.z);
+            Renderer rend = part.GetComponent<Renderer>();
+            rend.material.SetColor("_Color", c);
+            particles[p] = part;
+            pScript.color = materialColors;
+            pScript.g = 9.81f;
+        
+            // + Each particle's mass will equal the radius times 2
+            pScript.m = pScript.r * 2.0f;
+
+            pScript.restitution = 0.9f; 
+            pScript.f = Vector3.zero;
+            pScript.f.y = -pScript.m * pScript.g;
+            // + The particles will explode from the emitter at(0,0, 0) with random forces in ±X ±Y and±Z.
+            pScript.currPos = new Vector3(0,15,0); //
+            pScript.f.x += Random.Range(-1.5f, 1.5f);
+            pScript.f.y += Random.Range(-1.5f, 1.5f);
+            pScript.f.z += Random.Range(-1.5f, 1.5f);
+
+            pScript.prevPos = pScript.currPos;
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Time.realtimeSinceStartup > 3.0f){      
+            for(int i = 0; i < numParticles; i++){
+                bool crashed = false;
+                Particle p1 = particles[i].GetComponent<Particle>();
+                for(int j = i+1; j < numParticles; j++){       
+                    Particle p2 = particles[j].GetComponent<Particle>();
+                    if(p1.InCollision(p2)){
+                        // +When two particles collide, they will change theirdiffuse color tored.
+                        //Change p1 to RED
+                        crashed = true;
+                        Renderer rend = p1.GetComponent<Renderer>();
+                        rend.material.SetColor("_Color", Color.red);
+                        //Change p2 to RED
+                        rend = p2.GetComponent<Renderer>();
+                        rend.material.SetColor("_Color", Color.red);
+                        //Debug.Log("Collision PUM: " + i.ToString() + " vs " + j.ToString());
+                    }
+                }
+                if(!crashed){
+                    // +When any particle is not in collision, it will returnto itsoriginal color.
+                    Color c = new Color(p1.color.x, p1.color.y, p1.color.z);
+                    Renderer rend = p1.GetComponent<Renderer>();
+                    rend.material.SetColor("_Color", c);
+                }
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Particle.cs
@@ -0,0 +1,134 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Particle : MonoBehaviour
+{
+    public float r;
+    public float g;
+    public float m;
+    public Vector3 f;
+    public Vector3 a;
+    public Vector3 prevPos;
+    public Vector3 currPos;
+    public Vector3 airForce;
+    public float restitution;
+    public Vector3 color;
+
+    
+    // Start is called before the first frame update
+    void Start(){
+    }
+
+    /* When particles collide against a wall of the imaginarycube, they will
+    bounce in thedirection of the reflected ray around the cube’s wall internal normal vector 
+    (the normalpointing inside the cube).
+
+    */
+    // void CheckWalls(){ //Check in x
+    //     if(currPos.x > 10-r){ //Right Wall
+    //         prevPos.x = currPos.x;
+    //         currPos.x = 10f-r;
+    //         f.x = -f.x * restitution;
+    //         a = f / m;
+    //     }
+
+    //     if(currPos.x < -10+r){ //Left Wall
+    //         prevPos.x = currPos.x;
+    //         currPos.x = -10+r;
+    //         f.x = -f.x * restitution;
+    //         a = f / m;
+    //     }
+    // }
+
+    // void CheckFloorCeil(){ //Check in y
+    //     if(currPos.y > 10-r){ //Ceil
+    //         prevPos.y = currPos.y;
+    //         currPos.y = 10-r;
+    //         f.y = -f.y * restitution;
+    //         a = f / m;
+    //     }
+
+    //     if (currPos.y < -10+r){ //Floor
+    //         prevPos.y = currPos.y;
+    //         currPos.y = -10+r;
+    //         f.y = -f.y * restitution;
+    //         a = f / m;
+    //     }    
+
+    // }
+
+    void CheckFloor(){
+        if (currPos.y < -10+r){ //Floor
+            prevPos.y = currPos.y;
+            currPos.y = -10+r;
+            f.y = -f.y * restitution;
+            a = f / m;
+        }  
+    }
+
+    // void CheckBackFront(){ //Check in z
+    //     if(currPos.z > 10-r){ //Front
+    //         prevPos.z = currPos.z;
+    //         currPos.z = 10-r;
+    //         f.z = -f.z * restitution;
+    //         a = f / m;
+    //         //Debug.Log("Ceil?");
+    //     }
+
+    //     if (currPos.z < -10+r){ //Back
+    //         prevPos.z = currPos.z;
+    //         currPos.z = -10+r;
+    //         f.z = -f.z * restitution;
+    //         a = f / m;
+    //     }
+
+    // }
+    // Update is called once per frame
+    void Update()
+    {
+        if(Mathf.Abs(currPos.y - prevPos.y) < 0.00001f && Mathf.Abs(currPos.y - r) < 0.00001f){
+            currPos.y = r;
+            prevPos.y = r;
+            f.y = 0;
+        }else{
+            f.y = -m * g;
+            if(currPos.y != prevPos.y){
+                Vector3 vel = (currPos - prevPos) / Time.deltaTime;
+                if(currPos.y > prevPos.y){
+                    f.y = f.y - r * 0.001f * vel.magnitude;
+                }else if (currPos.y < prevPos.y){
+                    f.y = f.y + r * 0.001f * vel.magnitude;
+                }
+            }
+        }
+        a = f/m;
+        Vector3 temp = currPos;
+        float dt = Time.deltaTime;
+        if(Time.frameCount > 100){
+            // +Particles will be simulated using Verlet's integration technique
+            currPos = 2 * currPos - prevPos + a * dt * dt; //Verlets
+            prevPos = temp;
+            // CheckWalls();
+            // CheckFloorCeil();
+            // CheckBackFront();
+            CheckFloor();
+
+        }
+        transform.localPosition = currPos;
+    }
+
+    public bool InCollision(Particle antoher){
+        float sumR2 = r + antoher.r;
+        sumR2 *= sumR2;
+        Vector3 c1 = transform.localPosition; //C1
+        Vector3 c2 = antoher.transform.localPosition; //C2
+        float dx = c2.x - c1.x;
+        float dy = c2.y - c1.y;
+        float dz = c2.z - c1.z;
+        float d2 = dx * dx + dy * dy + dz * dz;
+        return d2 < sumR2;
+    }
+
+
+}
